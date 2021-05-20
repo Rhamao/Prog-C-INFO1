514c16fd00fe48ed894cf43fffad361dbb4d4a0e
APELBAUM ARNAUD <aapelbau@localhost.localdomain>
2021-05-20 11:03:53 +0200

    update ex1 tp10

---
+++ 10tp/1ex/main.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <malloc.h>
+
+/**
+ *Écrire une fonctionvoid swap_mem(void* z1, void* z2, size_t size)
+ *qui échange lesdonnées binaires contenues dans les zonesz1etz2. 
+ *Tester votre fonction sur des entiers, desdoubles, des tableaux 
+ *voire des structures de votre choix.
+ */  
+void swap_mem(void* z1, void* z2, size_t size){
+	return;
+}
+int main(int argc, char *argv[]){
+
+	
+	
+
+
+	return EXIT_SUCCESS;
+}
