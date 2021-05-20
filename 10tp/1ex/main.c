fcc79d325a6e180a95d17e4a240649dea12eedad
APELBAUM ARNAUD <aapelbau@localhost.localdomain>
2021-05-20 11:22:57 +0200

    completed ex1 tp10

--- 10tp/1ex/main.c
+++ 10tp/1ex/main.c
@@ -1,21 +1,34 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <malloc.h>
+#include <string.h>
 
-/**
- *Écrire une fonctionvoid swap_mem(void* z1, void* z2, size_t size)
- *qui échange lesdonnées binaires contenues dans les zonesz1etz2. 
- *Tester votre fonction sur des entiers, desdoubles, des tableaux 
- *voire des structures de votre choix.
- */  
-void swap_mem(void* z1, void* z2, size_t size){
-	return;
-}
+void swap_mem(void* z1, void* z2, size_t size);
+
 int main(int argc, char *argv[]){
 
-	
-	
+	int a = 1, b = 2;
+	double aa = 1.0, bb = 2.0;
+	double aaa = 1.0, bbb = 2.0;  
 
+	swap_mem(&a, &b, sizeof(int));
+	swap_mem(&aa, &bb, sizeof(double));
+	swap_mem(&aaa, &bbb, sizeof(double));
 
+	printf("%d %d\n", a, b);
+	printf("%f %f\n", aa, bb);
+	printf("%f %f\n", aaa, bbb);	
+	
 	return EXIT_SUCCESS;
+}
+
+void swap_mem(void* z1, void* z2, size_t size){
+	void* buff = malloc(size);
+	if (buff == NULL){
+        printf("Cannot allocate memory");
+        return;
+    }
+    memcpy(buff, z2, size); 
+    memcpy(z2, z1, size);
+    memcpy(z1, buff, size);
 }
